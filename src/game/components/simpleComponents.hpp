a0f1beb178f2ba7b5dbcb0b69cde7c5f6f43477b
Julian Benda <julian.benda@ovgu.de>
2020-03-28 16:06:57 +0100

    Add constructor from Base type for simple Components
    Default constructor and base type constructor are ambitions
    -> base type constructor are deleted because of auto constructed
    default constructor

--- src/game/components/simpleComponents.hpp
+++ src/game/components/simpleComponents.hpp
@@ -8,21 +8,29 @@ namespace game {
 	struct Position : public glm::vec3
 	{
 		using glm::vec3::vec3;
+		Position(const glm::vec3& v) : glm::vec3{v}{} 
+		Position(glm::vec3&& v) : glm::vec3{v}{} 
 	};
 
 	struct Velocity : public glm::vec3
 	{
 		using glm::vec3::vec3;
+		Velocity(const glm::vec3& v) : glm::vec3{v}{}
+		Velocity(glm::vec3& v) : glm::vec3{v}{}
 	};
 
 	struct Rotation : public glm::quat
 	{
 		using glm::quat::quat;
+		Rotation(const glm::quat& q) : glm::quat{q}{}
+		Rotation(glm::quat&& q) : glm::quat{q}{}
 	};
 
 	struct Transform : public glm::mat4
 	{
 		using glm::mat4::mat4;
+		Transform(const glm::mat4& m) : glm::mat4{m}{}
+		Transform(glm::mat4&& m) : glm::mat4{m}{}
 	};
 
 	struct Lifetime
