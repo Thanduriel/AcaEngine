2ceb0173e0762e5d9338329ceaae15ece265cc60
JBenda <Zabuza98@gmx.net>
2021-01-20 14:56:39 +0100

    Make struct constructor explicit

--- src/engine/game/operations/addPointLights.hpp
+++ src/engine/game/operations/addPointLights.hpp
@@ -23,7 +23,7 @@ namespace game { namespace operations {
 
 		void operator()(const components::Position& _pos, const components::BoundingBox& _bb, const components::PointLight& _pl) const {
 			math::AABB<3,float> aabb(_pos.value + _bb.min, _pos.value + _bb.max);
-			m_octree.insert(aabb, PointLight(_pl.color,_pos.value,_pl.intensity));
+			m_octree.insert( aabb, PointLight{ .color = _pl.color, .position = _pos.value,.intensity = _pl.intensity } );
 		}
 	private:
 		octree_t& m_octree; ///< octree containing id and bounds of ligths
