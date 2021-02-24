3d7e0dec6921d8a60aa378be1364610e803f5f07
Thanduriel <rojendersie@alice.de>
2021-02-24 18:19:12 +0100

    + World supports generic resources as template params

--- src/engine/game/core/world.hpp
+++ src/engine/game/core/world.hpp
@@ -4,21 +4,29 @@
 
 namespace game {
 
+	template<typename... Resources>
 	class World
 	{
 	public:
+		World(Resources&&... _resources) 
+			: m_resources(std::forward<Resources>(_resources)...),
+			m_deltaTime(0.f) {}
+
 		void process(float _deltaTime)
 		{
 			m_deltaTime = _deltaTime;
 		}
 
+		// direct resource access; should not be used
 		Registry2& getRegistry() { return m_registry; }
+		template<typename Resource>
+		Resource& getResource() { return std::get<Resource>(m_resources); }
 
 		// Call member function of a system providing the expected resources.
-		template<typename System, typename... Resources>
-		void call(System& _system, void(System::* _func)(Resources ...)) { (_system.*_func)(ResourceFetch<Resources>::get(*this)...); }
-		template<typename System, typename... Resources>
-		void call(const System& _system, void(System::* _func)(Resources ...) const) { (_system.*_func)(ResourceFetch<Resources>::get(*this)...); }
+		template<typename System, typename... ResourcesReq>
+		void call(System& _system, void(System::* _func)(ResourcesReq ...)) { (_system.*_func)(ResourceFetch<ResourcesReq>::get(*this)...); }
+		template<typename System, typename... ResourcesReq>
+		void call(const System& _system, void(System::* _func)(ResourcesReq ...) const) { (_system.*_func)(ResourceFetch<ResourcesReq>::get(*this)...); }
 
 		template<typename System>
 		void run(System& _system) { call(_system, &System::update); }
@@ -26,8 +34,15 @@ namespace game {
 		template<typename System>
 		void run(const System& _system) { call(_system, &System::update); }
 	private:
+		// generic lookup in the resource tuple
 		template<typename Resource>
-		struct ResourceFetch;
+		struct ResourceFetch
+		{
+			static Resource& get(World& world)
+			{
+				return std::get<std::remove_cvref_t<Resource>>(world.m_resources);
+			}
+		};
 
 		// game time
 		template<>
@@ -71,5 +86,6 @@ namespace game {
 
 		float m_deltaTime;
 		Registry2 m_registry;
+		std::tuple<Resources...> m_resources;
 	};
 }
